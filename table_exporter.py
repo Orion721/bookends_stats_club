0e8d62ac43bc80322c4b55f64e161207d74bc4e3
DESKTOP-SP15I41\hunte <huntersanders721@gmail.com>
2020-10-23 19:19:27 -0400

    Added all dataframe-sql table methods. Commented out to improve runtime

--- table_exporter.py
+++ table_exporter.py
@@ -44,8 +44,62 @@ class Export_Table:
                                    'games_played': sqlalchemy.types.INTEGER(),
                                    'wins': sqlalchemy.types.INTEGER(),
                                    'losses': sqlalchemy.types.INTEGER()})
+                                   
+    def export_team_event_events(self):
+        self.team_event_events_df.to_sql(name='team_event_events',
+                                        con=self.db_connection,
+                                        if_exists='replace',
+                                        index=True,
+                                        index_label='team_event_id',
+                                        dtype={'event_id': sqlalchemy.types.INTEGER()})
+    
+    def export_team_event_event_pools(self):
+        self.team_event_event_pools_df.to_sql(name='team_event_event_pools',
+                                        con=self.db_connection,
+                                        if_exists='replace',
+                                        index=True,
+                                        index_label='event_pool_id',
+                                        dtype={'team_event_id': sqlalchemy.types.INTEGER()})
+
+    def export_event_pools(self):
+        self.event_pools_df.to_sql(name='event_pools', 
+                            con=self.db_connection, 
+                            if_exists='replace', 
+                            index=True, 
+                            index_label='event_pool_id',
+                            dtype={'pool': sqlalchemy.types.CHAR(length=100), 
+                                   'pool_start': sqlalchemy.types.INTEGER(), 
+                                   'pool_finish': sqlalchemy.types.INTEGER(), 
+                                   'pool_wins': sqlalchemy.types.INTEGER(), 
+                                   'pool_losses': sqlalchemy.types.INTEGER()})
+
+    def export_pool_games(self):
+        self.pool_games_df.to_sql(name='pool_games', 
+                            con=self.db_connection, 
+                            if_exists='replace', 
+                            index=True, 
+                            index_label='pool_games_id',
+                            dtype={'event_day_id': sqlalchemy.types.INTEGER(), 
+                                   'game_time': sqlalchemy.types.time(), 
+                                   'first_team_event_id': sqlalchemy.types.INTEGER(), 
+                                   'first_team_score': sqlalchemy.types.DECIMAL(), 
+                                   'second_team_event_id': sqlalchemy.types.INTEGER(),
+                                   'second_team_score': sqlalchemy.types.INTEGER()})
+    
+    def export_bracket_games(self):
+        self.pool_games_df.to_sql(name='bracket_games', 
+                            con=self.db_connection, 
+                            if_exists='replace', 
+                            index=True, 
+                            index_label='bracket_games_id',
+                            dtype={'bracket_type_id': sqlalchemy.types.INTEGER(), 
+                                   'bracket_game_type_id': sqlalchemy.types.INTEGER(), 
+                                   'event_day_id': sqlalchemy.types.INTEGER(), 
+                                   'game_time': sqlalchemy.types.time(), 
+                                   'first_team_event_id': sqlalchemy.types.INTEGER(),
+                                   'first_team_score': sqlalchemy.types.INTEGER()})
 '''
-#Export_Table().export_teams()
+Export_Table().export_teams()
 
 #Teams
 #Team_Team_Events
