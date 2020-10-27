c6999f7be1bf03e8dc15c134bc108e66596cdfa3
DESKTOP-SP15I41\hunte <huntersanders721@gmail.com>
2020-10-27 12:59:49 -0400

    Increased efficiency for get_team_ids, added more team name cleanup

--- team_team_events.py
+++ team_team_events.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from collections import Counter
 from teams import Teams, Full_Seeds_Pools_Info
 
 class Team_Team_Events:
@@ -9,26 +10,27 @@ class Team_Team_Events:
     def team_ids_dict(self):
         team_ids_dict = dict(zip(list(Teams().teams_dataframe()['team_name']), list(Teams().teams_dataframe().index)))
         return team_ids_dict
-    
+
     def clean_teams(self):
         teams_raw = []
         for event in self.teams:
             teams_raw.extend(event)
         team_cleaner = {'OdyssÃ©e': 'Odyssée', 'OmertÃƒ': 'OmertÃ', 'PanamÃƒÂ¡': 'PanamÃ¡', 
                         'Chad Larson Experience': 'The Chad Larson Experience', 'Chicago Machine': 'Machine', 
-                        'Seattle Riot': 'Riot', 'Revolution (Columbia)': 'Revolution', 'Jack Wagon': 'Jackwagon',
+                        'Seattle Riot': 'Riot', 'Revolution (Colombia)': 'Revolution', 'Jack Wagon': 'Jackwagon',
                         'Club Deportivo Revolution': 'Revolution', 'MesteÃ±o': 'Mesteño', 'SoCal Condors': 
-                        'Condors', 'American BBQ': 'American Barbecue', 'Boost Mobile': 'Boost FC', 'Kie': 'K.ie', 
+                        'Condors', 'American BBQ': 'American Barbecue', 'Boost Mobile': 'Boost FC', 'Kie': 'Ki.e', 
                         'The DOH Abides': "D'oh! Abides", '7Express': '7 Express', 'Seattle Mixed': 'Seattle Mixtape', 
                         'Pittsburgh Temper': 'Temper', 'BirdFruit': 'Birdfruit', 'Richmond Floodwall': 'Floodwall',
-                        'Bent': 'BENT', 'Rhino Slam': 'Rhino Slam!', 'Portland Ivy': 'Ivy', 'Freaks Uv Nature': 'Freaks', 'CLE Smokestack': 'Smokestack'}
+                        'Bent': 'BENT', 'Rhino Slam': 'Rhino Slam!', 'Portland Ivy': 'Ivy', 'Freaks Uv Nature': 'Freaks', 
+                        'MesteÃƒÂ±o': 'Mesteño', 'CLE Smokestack': 'Smokestack', 'Dig': 'DiG', 'Pleasuretown': 'PleasureTown'}
         clean_teams = [team_cleaner.get(x,x) for x in teams_raw]
         return clean_teams
 
     def get_team_ids(self):
-        team_ids = [self.team_ids_dict()[x] for x in self.clean_teams() if x in self.team_ids_dict()]
+        team_ids = list(map(self.team_ids_dict().get, self.clean_teams()))
         return team_ids
-    
+
     def team_team_events_df(self):
         cols = {'team_id': self.get_team_ids()}
         df = pd.DataFrame(data=cols)
